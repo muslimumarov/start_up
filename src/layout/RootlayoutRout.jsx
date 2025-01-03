18be0caab2de17df7f5131c352a9b24c4d322c90
muslimumarov <muslimbekaxmatovich@gmail.com>
2025-01-03 06:12:19 +0500

    ...dkfldkf

--- src/layout/RootlayoutRout.jsx
+++ src/layout/RootlayoutRout.jsx
@@ -1,19 +1,28 @@
 import { NavLink, Outlet } from "react-router-dom";
-
+import { CiUser } from "react-icons/ci";
 export default function RootlayoutRout() {
   return (
     <>
       <header>
         <nav>
-          <NavLink className={`bg-transparent`} to="/">home</NavLink>
-          <NavLink className={`bg-transparent`} to="/course">Kurslar</NavLink>
-          <NavLink className={`bg-transparent mr-[323px]`} to="/help">Yordam</NavLink>
-          <NavLink className={`bg-transparent`} to="/kirish">Kirish</NavLink>
-          <NavLink className={`bg-transparent`} to="/ro'yhat">Royhatdan otish</NavLink>
+          <NavLink to="/">home</NavLink>
+          <NavLink to="/course">Kurslar</NavLink>
+          <NavLink className={`mr-[323px]`} to="/help">
+            Yordam
+          </NavLink>
+          <a href="/kirish" className="bg-transparent">
+            <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+              Kirish
+            </button>
+          </a>
+          <a href="/ro'yhat" className="bg-transparent border border-[#19A6F5] border hover:border-[2px] rounded-[5px]">
+            <CiUser />
+            Ro&#39;yhatdan o&#39;tish
+          </a>
         </nav>
       </header>
       <main>
-        <Outlet/>
+        <Outlet />
       </main>
       <footer></footer>
     </>
